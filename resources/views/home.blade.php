c3f776ea381d79bca19abcfc90b259ee37098ae4
Sakith Enuka <sakith.enuka2002@gmail.com>
2023-09-08 18:57:46 +0530

    Entered References

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -420,6 +420,12 @@
     <script src="{{ asset('public/register.js') }}"></script>
     <script src="{{ asset('public/ogin.js') }}"></script>
 
+    <!-- Reference
+
+    Pin on Brad♥️✨ (2022) Pinterest. Available at: https://www.pinterest.com/pin/163466661466827524/ (Accessed: 08 September 2023). 
+    Pergola, M.L. (2022) Photo by Mario La Pergola on unsplash, A bunch of items that are on display photo – Free Guitar Image on Unsplash. Available at: https://unsplash.com/photos/Wrx0iVcYKmM (Accessed: 08 September 2023). 
+    We love music (no date) We Love Music. Available at: https://www.welovemusic.lk/ (Accessed: 08 September 2023).  -->
+
 
 </body>
 
